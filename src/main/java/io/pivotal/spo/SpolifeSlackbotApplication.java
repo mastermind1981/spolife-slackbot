e36be48bbdddbb916fdd93b107642c48272e1009
Matt Stine <mstine@gopivotal.com>
2016-08-28 21:57:30 -0400

    initial commit from template

---
+++ src/main/java/io/pivotal/spo/SpolifeSlackbotApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.spo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpolifeSlackbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpolifeSlackbotApplication.class, args);
+	}
+}
